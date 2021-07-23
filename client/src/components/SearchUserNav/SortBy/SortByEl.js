aa704ab0eabbd2cddfa42b945b5d796bf203b65a
Anisha McDowell <anishamcdowell@gmail.com>
2021-07-23 16:48:10 -0400

    mobile search user nav responding correctly at all sizes

--- client/src/components/SearchUserNav/SortBy/SortByEl.js
+++ client/src/components/SearchUserNav/SortBy/SortByEl.js
@@ -1,13 +1,14 @@
-import React from 'react'
+import React from 'react';
+import "./sortbyel.css";
 
 function SortBy() {
     return (
-        <div class="job-filter mb-4 d-sm-flex align-items-center">
-            <div class="job-shortby ml-sm-auto d-flex align-items-center">
+
+            <div class="job-sortby">
               <form class="form-inline">
-                <div class="form-group mb-0">
-                  <label class="justify-content-start mr-2">Sort by :</label>
-                  <div class="short-by">
+
+                  <label class="mr-2" id="sort-by-text">Sort by :</label>
+                  <span class="sort-by" id="sort-by-input">
                     <select
                       class="form-control basic-select select2-hidden-accessible"
                       data-select2-id="1"
@@ -17,11 +18,11 @@ function SortBy() {
                       <option data-select2-id="3">Newest</option>
                       <option>Oldest</option>
                     </select>
-                  </div>
-                </div>
+                  </span>
+
               </form>
             </div>
-          </div>
+
     )
 }
 
