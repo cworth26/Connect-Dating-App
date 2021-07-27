c828218b02db669e5861dcee5abb629d43bb4d3d
Tierra Rogers <tierragore@yahoo.com>
2021-07-26 22:26:00 -0400

    Added pop up message

--- client/src/pages/Help/index.js
+++ client/src/pages/Help/index.js
@@ -10,55 +10,82 @@ const Help = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
-  
+  const [send, setSend] = useState("");
+
+  const onSubmit = () => {
+    alert(`Thank you for you message.`);
+  };
+
   function sendEmail(e) {
     e.preventDefault();
     emailjs
       .sendForm(
-        "service_upld0n3",
-        "template_vyjd6k4",
+        "service_wzvldih",
+        "template_ukqwsoc",
         e.target,
-        "user_VGRqbXjGwdNwwhPNY7iVS"
+        "user_fMy7PDcJVzZgYPmJPBW9B"
       )
       .then((res) => {
         setName("");
         setEmail("");
         setMessage("");
+        setSend("");
         console.log(res);
       })
       .catch((err) => console.log(err));
   }
   return (
-      <div
-        className="container border"
-        style={{
-          marginTop: "50px",
-          width: "50%",
-          backgroundImage: `url('https://cdn.pixabay.com/photo/2016/08/23/10/45/network-1614045_960_720.png')`,
-          backgroundPosition: "center",
-          backgroundSize: "cover",
-        }}
+    <div
+      className="container border"
+      style={{
+        marginTop: "50px",
+        width: "50%",
+        backgroundImage: `url('https://cdn.pixabay.com/photo/2016/08/23/10/45/network-1614045_960_720.png')`,
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+      }}
+    >
+      <h1 style={{ marginTop: "25px" }}>Connect with us</h1>
+      <form
+        className="row"
+        style={{ margin: "25px 85px 75px 100px" }}
+        onSubmit={sendEmail}
       >
-        <h1 style={{ marginTop: "25px" }}>Connect with us</h1>
-        <form
-          className="row"
-          style={{ margin: "25px 85px 75px 100px" }}
-          onSubmit={sendEmail}
-        >
-          <label>Name</label>
-          <input onChange={e => setName(e.target.value)} value={name} type="text" name="name" />
-          <label>Email</label>
-          <input onChange={e => setEmail(e.target.value)} value={email} type="email" name="user_email" className="form-control" />
-          <label>Message</label>
-          <textarea onChange={e => setMessage(e.target.value)} value={message} type="message" rows="4" className="form-control" />
-          <input
-            type="submit"
-            value="Send"
-            className="form-control btn btn-secondary"
-            style={{ marginTop: "30px" }}
-          />
-        </form>
-      </div>
+        <label>Name</label>
+        <input
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+          type="text"
+          name="name"
+        />
+        <label>Email</label>
+        <input
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          type="email"
+          name="user_email"
+          className="form-control"
+        />
+        <label>Message</label>
+        <textarea
+          onChange={(e) => setMessage(e.target.value)}
+          value={message}
+          type="message"
+          name="message"
+          rows="4"
+          className="form-control"
+        />
+        
+        <input
+          onClick={onSubmit}
+          onChange={(e) => setSend(e.target.value)}
+          type="submit"
+          value="Send"
+          className="form-control btn btn-secondary"
+          style={{ marginTop: "30px" }}
+        />
+      </form>
+    </div>
   );
 };
 
